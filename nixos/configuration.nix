a0dd76d70347dafd6189ca3fed1d245a33cb0945
Devon Richards <friends.devon@gmail.com>
2023-08-03 13:16:32 -0500

    add postgres for local testing.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -202,6 +202,18 @@
       pulse.enable = true;
       jack.enable = false;
     };
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ];
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  auth-method
+        local all       all     trust
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
+      '';
+    };
     timesyncd.enable = true;
     uptimed.enable = true;
     xserver = {
